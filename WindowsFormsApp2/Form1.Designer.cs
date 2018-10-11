9384110e0c8b4e8e3ba36ac3c67f22e51f7448d5
Max <37274332+feracios120499@users.noreply.github.com>
2018-10-11 17:11:03 +0300

    11

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -30,11 +30,12 @@
         {
             this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
             this.cartesianChart1 = new LiveCharts.Wpf.CartesianChart();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // elementHost1
             // 
-            this.elementHost1.BackColor = System.Drawing.SystemColors.Control;
+            this.elementHost1.BackColor = System.Drawing.Color.Transparent;
             this.elementHost1.Location = new System.Drawing.Point(118, 97);
             this.elementHost1.Name = "elementHost1";
             this.elementHost1.Size = new System.Drawing.Size(881, 313);
@@ -42,12 +43,25 @@
             this.elementHost1.Text = "elementHost1";
             this.elementHost1.Child = this.cartesianChart1;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(384, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(1042, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.elementHost1);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -59,6 +73,7 @@
 
         private System.Windows.Forms.Integration.ElementHost elementHost1;
         private LiveCharts.Wpf.CartesianChart cartesianChart1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -22,24 +22,78 @@ namespace WindowsFormsApp2
         public Form1()
         {
             InitializeComponent();
-            List<Pollution> pollutions = new List<Pollution>()
-            {
-                new Pollution{Location="1",LastUpdate=Convert.ToDateTime("12.04.2018"),Value=0.16},
-                new Pollution{Location="2",LastUpdate=Convert.ToDateTime("12.04.2018"),Value=0.14},
-                new Pollution{Location="1",LastUpdate=Convert.ToDateTime("13.04.2018"),Value=0.17},
-                new Pollution{Location="2",LastUpdate=Convert.ToDateTime("14.04.2018"),Value=0.16},
-                new Pollution{Location="1",LastUpdate=Convert.ToDateTime("14.04.2018"),Value=0.13}
+            
 
-            };
+
+        }
+        public class ComparerDate : IComparer<string>
+        {
+            public int Compare(string x, string y)
+            {
+                if (Convert.ToDateTime(x) == Convert.ToDateTime(y))
+                    return 0;
+                if (Convert.ToDateTime(x) < Convert.ToDateTime(y))
+                    return -1;
+                return 1;
+            }
+        }
+        private void CartesianChart1OnDataClick(object sender, ChartPoint chartPoint)
+        {
+            MessageBox.Show("You clicked (" + chartPoint.X + "," + chartPoint.Y + ")");
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            
+            var result = await Get("US");
+            Init(result.ToList());
+        }
+
+        private static string url = $"https://api.openaq.org/v1/latest?limit=10&city={"St. Louis"}&parameter=o3";
+        private static readonly HttpClient client = new HttpClient();
+        public async static Task<IEnumerable<Pollution>> Get(string country)
+        {
+            List<Pollution> pollutions = new List<Pollution>();
+            var response = await client.GetAsync(url + "&country=" + country);
+            var responseString = await response.Content.ReadAsStringAsync();
+            JObject json = JObject.Parse(responseString);
+            var token = json["results"];
+            foreach (var item in token.Children())
+            {
+                var pollution = JsonConvert.DeserializeObject<Pollution>(item["measurements"].First.ToString());
+                pollution.City = item["city"].ToString();
+                pollution.Location = item["location"].ToString();
+                pollutions.Add(pollution);
+            }
+            return pollutions;
+        }
+
+        public void Init(List<Pollution> pollutions)
+        {
+            //List<Pollution> pollutions = new List<Pollution>()
+            //{
+            //    new Pollution{Location="ALTON",LastUpdate=Convert.ToDateTime("11.10.2018"),Value=0.017},
+            //    new Pollution{Location="ARNOLD",LastUpdate=Convert.ToDateTime("11.10.2018"),Value=0.011},
+            //    new Pollution{Location="Alhambra",LastUpdate=Convert.ToDateTime("11.10.2018"),Value=0.014},
+            //    new Pollution{Location="BLAIR STREET",LastUpdate=Convert.ToDateTime("11.10.2018"),Value=0.015},
+            //    new Pollution{Location="East St.Louis",LastUpdate=Convert.ToDateTime("11.10.2018"),Value=0.016},
+            //    new Pollution{Location="Foley",LastUpdate=Convert.ToDateTime("07.11.2016"),Value=0.046},
+            //    new Pollution{Location="Jorey",LastUpdate=Convert.ToDateTime("20.07.2017"),Value=0.05},
+            //    new Pollution{Location="MARVYL",LastUpdate=Convert.ToDateTime("11.10.2018"),Value=0.013},
+            //    new Pollution{Location="Maryland",LastUpdate=Convert.ToDateTime("11.10.2018"),Value=0.017},
+            //    new Pollution{Location="NYLWOOD",LastUpdate=Convert.ToDateTime("11.10.2018"),Value=0.018}
+
+            //};
             var map = Mappers.Xy<DataModel>().X(p => p.Number).Y(p => p.Value);
             var dates = pollutions.Select(p => p.LastUpdate.ToShortDateString()).ToList();
             dates = dates.Distinct().ToList();
+            dates.Sort(new ComparerDate());
             foreach (var item in pollutions)
             {
                 if (!cartesianChart1.Series.Any(p => p.Title == item.Location))
                 {
                     cartesianChart1.Series.Add(
-                        new LineSeries(map)
+                        new ColumnSeries(map)
                         {
                             Title = item.Location,
                             Values = new ChartValues<DataModel>
@@ -60,56 +114,14 @@ namespace WindowsFormsApp2
                          new DataModel { Value = item.Value, Number = dates.FindIndex(p => p == item.LastUpdate.ToShortDateString()) });
                 }
             }
-
+            cartesianChart1.AxisX.Clear();
             cartesianChart1.AxisX.Add(new Axis { Labels = dates });
-            //cartesianChart1.AxisY.Add(new Axis
-            //{
-            //    Title = "Sold Apps",
-            //    LabelFormatter = value => value.ToString("N")
-            //});
+            cartesianChart1.LegendLocation = LegendLocation.Right;
         }
 
-        private void CartesianChart1OnDataClick(object sender, ChartPoint chartPoint)
+        private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("You clicked (" + chartPoint.X + "," + chartPoint.Y + ")");
-        }
-
-        private async void Form1_Load(object sender, EventArgs e)
-        {
-            //var result = await Get("US");
-            //List<string> locations = new List<string>();
-            //cartesianChart1.LegendLocation = LegendLocation.Right;
-            //foreach (var item in result)
-            //{
-            //    cartesianChart1.Series.Add(new ColumnSeries
-            //    {
-            //        Title = item.Location,
-            //        Values = new ChartValues<double> { item.Value },
-            //    });
-
-            //    locations.Add(item.LastUpdate.ToShortDateString());
-            //}
-            //cartesianChart1.AxisX.FirstOrDefault().Labels = locations;
-            //cartesianChart1.AxisX.FirstOrDefault().ShowLabels = false;
-        }
-
-        private static string url = $"https://api.openaq.org/v1/latest?limit=10&city={"St. Louis"}&parameter=o3";
-        private static readonly HttpClient client = new HttpClient();
-        public async static Task<IEnumerable<Pollution>> Get(string country)
-        {
-            List<Pollution> pollutions = new List<Pollution>();
-            var response = await client.GetAsync(url + "&country=" + country);
-            var responseString = await response.Content.ReadAsStringAsync();
-            JObject json = JObject.Parse(responseString);
-            var token = json["results"];
-            foreach (var item in token.Children())
-            {
-                var pollution = JsonConvert.DeserializeObject<Pollution>(item["measurements"].First.ToString());
-                pollution.City = item["city"].ToString();
-                pollution.Location = item["location"].ToString();
-                pollutions.Add(pollution);
-            }
-            return pollutions;
+            
         }
     }
 }
